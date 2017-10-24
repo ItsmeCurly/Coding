a6d8995a71e55121e92931dbdf235a9c47672775
Adam <adamg765@gmail.com>
2017-10-23 23:39:38 -0400

    Coding update

--- IdeaProjects/GUI/src/Pane.java
+++ IdeaProjects/GUI/src/Pane.java
@@ -2,9 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.StringTokenizer;
 
 public class Pane extends JPanel implements ActionListener {
-    private final String ITEMS[] = {"c", "s", "e", "r", "d", "xs", "xh", "xp"};
+    private final String spacing1 = " ";
+    private final String spacing2 = "                 ";
+    private final String ITEMS[] = {"<none>", "c", "s", "e", "r", "d", "xs", "xh", "xp"};
     private JPanel lowerPane;
     private JPanel upperPane;
     private JComboBox<String> cb;
@@ -12,34 +15,55 @@ public class Pane extends JPanel implements ActionListener {
     private JTextField jt;
     private JLabel com;
     private JLabel args;
+    private JButton jb;
+
+    private int command;
+    private String text;
+
+    private StringTokenizer st;
 
     public Pane() {
+        command = -1;
+
         cb = new JComboBox<>(ITEMS);
         cb.addActionListener(this);
 
         rm = new RecordManager();
 
-        jt = new JTextField();
+        jt = new JTextField(2);
         jt.addActionListener(this);
 
-        com = new JLabel("Commands");
-        args = new JLabel("Arguments");
+        com = new JLabel("Commands" + spacing1);
+        args = new JLabel("Args" + spacing2);
 
         upperPane = new JPanel();
         lowerPane = new JPanel();
 
+        jb = new JButton("Run");
+
         upperPane.setLayout(new FlowLayout());
         lowerPane.setLayout(new FlowLayout());
 
+        createUI();
+
+        setLayout(new GridLayout(2, 3);
+
+        add(upperPane, "North");
+        add(lowerPane, "Center");
+    }
+
+    private void createUI() {
         upperPane.add(com);
         upperPane.add(args);
 
+        args.setVisible(false);
+
         lowerPane.add(cb);
+        lowerPane.add(jt);
+        lowerPane.add(jb);
 
-        setLayout(new BorderLayout());
-
-        add(upperPane, "North");
-        add(lowerPane, "South");
+        jt.setVisible(false);
+        jb.setVisible(false);
     }
 
     @Override
@@ -48,42 +72,88 @@ public class Pane extends JPanel implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getSource());
         if (e.getSource().equals(cb)) {
-            if (cb.getSelectedItem().equals("c")) {
-                //rm.makeNew(Integer.parseInt(st.nextToken()));
-                //System.out.println();
-
+            boolean show = false;
+            boolean hide = true;
+            if (cb.getSelectedItem().equals("<none>")) {
+                reveal(false, false, false);
+                command = -1;
+            } else if (cb.getSelectedItem().equals("c")) {
+                command = 0;
+                reveal(true, true, true);
             } else if (cb.getSelectedItem().equals("s")) {
-//                int key = Integer.parseInt(st.nextToken());
-//                String data = st.nextToken();
-//                rm.store(new TreeNode(key, data));
-//                System.out.println();
+                command = 1;
+                reveal(true, true, true);
             } else if (cb.getSelectedItem().equals("e")) {
-//                int key = Integer.parseInt(st.nextToken());
-//                System.out.println(rm.search(key));
+                command = 2;
+                reveal(true, true, true);
             } else if (cb.getSelectedItem().equals("r")) {
-//                try {
-//                    int key = Integer.parseInt(st.nextToken());
-//                    TreeNode find = rm.searchNode(key);
-//                    if (find != null) {
-//                        String data = find.getData();
-//                        System.out.print((data != null) ? data : "");
-//                    }
-//                } catch (NumberFormatException e) {
-//                    System.err.print("key input not as integer");
-//                } finally {
-//                    System.out.println();
-//                }
+                command = 3;
+                reveal(true, true, true);
             } else if (cb.getSelectedItem().equals("d")) {
-//                int key = Integer.parseInt(st.nextToken());
-//                rm.delete(key);
+                command = 4;
+                reveal(true, true, true);
             } else if (cb.getSelectedItem().equals("xs")) {
-//                System.out.println(rm.size());
+                command = 5;
+                reveal(false, false, true);
             } else if (cb.getSelectedItem().equals("xh")) {
-//                System.out.println(rm.height());
+                command = 6;
+                reveal(false, false, true);
             } else if (cb.getSelectedItem().equals("xp")) {
-//                System.out.println(rm);
+                command = 7;
+                reveal(false, false, true);
             }
+        } else if (e.getSource().equals(jt)) {
+            text = jt.getText();
+        } else if (e.getSource().equals(jb)) {
+            st = new StringTokenizer(text, " ");
+            int key;
+            String data;
+            switch (command) {
+                case 0:
+                    rm.makeNew(Integer.parseInt(st.nextToken()));
+                    break;
+                case 1:
+                    key = Integer.parseInt(st.nextToken());
+                    data = st.nextToken();
+                    rm.store(new TreeNode(key, data));
+                    break;
+                case 2:
+                    key = Integer.parseInt(st.nextToken());
+                    System.out.println(rm.search(key));
+                    break;
+                case 3:
+                    key = Integer.parseInt(st.nextToken());
+                    TreeNode find = rm.searchNode(key);
+                    if (find != null) {
+                        data = find.getData();
+                        System.out.print((data != null) ? data : "");
+                    }
+                    break;
+                case 4:
+                    key = Integer.parseInt(st.nextToken());
+                    rm.delete(key);
+                    break;
+                case 5:
+                    System.out.println(rm.size());
+                    break;
+                case 6:
+                    System.out.println(rm.height());
+                    break;
+                case 7:
+                    System.out.println(rm);
+                    break;
+
+            }
+            reveal(false, false, false);
         }
+        repaint();
+    }
+
+    private void reveal(boolean arg, boolean text, boolean button) {
+        args.setVisible(arg);
+        jt.setVisible(text);
+        jb.setVisible(button);
     }
 }
--- IdeaProjects/GUI/src/Window.java
+++ IdeaProjects/GUI/src/Window.java
@@ -4,18 +4,22 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 public class Window extends JFrame implements WindowListener {
-    final Dimension SCREENSIZE = Toolkit.getDefaultToolkit().getScreenSize();
+    final static Dimension SCREENSIZE = new Dimension(240, 80);
 
-    public static void main() {
+    public static void main(String[] args) {
         JFrame f = new JFrame();
         JPanel p = new Pane();
 
-        f.setTitle("K-Search Trees");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setTitle("Records");
+
         Container cp = f.getContentPane();
 
+        p.setPreferredSize(SCREENSIZE);
         cp.add(p);
 
         f.pack();
+        f.setLocationRelativeTo(null);
         f.setVisible(true);
     }
 
@@ -26,32 +30,30 @@ public class Window extends JFrame implements WindowListener {
 
     @Override
     public void windowClosing(WindowEvent e) {
+        setVisible(false);
+        dispose();
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        System.exit(0);
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
         dispose();
         System.exit(0);
     }
 
     @Override
-    public void windowClosed(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowIconified(WindowEvent e) {
-
-    }
-
-    @Override
     public void windowDeiconified(WindowEvent e) {
-
     }
 
     @Override
     public void windowActivated(WindowEvent e) {
-
     }
 
     @Override
     public void windowDeactivated(WindowEvent e) {
-
     }
 }
