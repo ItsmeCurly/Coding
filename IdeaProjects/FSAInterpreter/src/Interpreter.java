b75ed4519d2ec90f8bb37d4891882b953fe5f98a
Adam <adamg765@gmail.com>
2018-02-12 18:41:06 -0500

    Automata COS 451 HW 2

--- IdeaProjects/FSAInterpreter/src/Interpreter.java
+++ IdeaProjects/FSAInterpreter/src/Interpreter.java
@@ -1,2 +1,72 @@
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Interpreter {
+    public Interpreter() {
+        Scanner scan = new Scanner(System.in);
+        Map<String, Object> varMap = new LinkedHashMap<>();
+        label:
+        while (true) {
+            switch (scan.next()) {
+                case "quit":
+                    break label;
+                case "print": {
+                    System.out.println(varMap.get(scan.next()));
+                    break;
+                }
+                case "define":
+                    String name = scan.next();
+                    String data = scan.nextLine().trim();
+
+                    if (data.equals("fsa")) {
+                        Automata fsa = new Automata();
+                        fsa.setDesc(scan.nextLine());
+                        String line = scan.nextLine();
+                        Scanner fsaScan = new Scanner(line);
+                        while (fsaScan.hasNext())
+                            fsa.addAlphabet(fsaScan.next().trim().charAt(0));
+                        ArrayList<ArrayList<String>> states = new ArrayList<>();
+                        while (!(line = scan.nextLine()).isEmpty()) {
+                            fsaScan = new Scanner(line);
+                            ArrayList<String> state = new ArrayList<>();
+                            while (fsaScan.hasNext()) {
+                                String s = fsaScan.next();
+                                if (s.contains("*")) {
+                                    s = s.substring(1);
+                                    fsa.addAccept(s);
+                                }
+                                state.add(s);
+                            }
+                            states.add(state);
+                        }
+                        fsa.setTransitions(states);
+                        varMap.put(name, fsa);
+                    } else if (data.contains("\"")) {
+                        varMap.put(name, data.substring(1, data.length() - 1));
+                    }
+
+                    break;
+                case "run": {
+                    Automata fsa = (Automata) varMap.get(scan.next());
+                    String x = scan.next();
+                    if (x.contains("\"")) {
+                        fsa.run(x);
+                    } else if (varMap.containsKey(x)) {
+                        fsa.run((String) varMap.get(x));
+                    }
+
+                    break;
+                }
+                default:
+                    System.err.println("Invalid input");
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Interpreter();
+    }
 }
