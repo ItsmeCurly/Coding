90e0eec22c222530dd157d13b367f75033aa63db
ItsmeCurly <31974406+ItsmeCurly@users.noreply.github.com>
2018-11-25 16:31:23 -0500

    Added multiple puzzle support

--- GoProjects/SudokuSolver/solver.go
+++ GoProjects/SudokuSolver/solver.go
@@ -9,73 +9,69 @@ import (
 	"time"
 )
 
-var prevValue [][]int
-var table [][]int
-var orig [][]bool
+var prevValue [][][]int
+var table [][][]int
+var orig [][][]bool
 
-const InputFileName string = "input2.txt"
+const InputFileName string = "input.txt"
 
 func main() {
-	table = make([][]int, 9)
-	orig = make([][]bool, 9)
-	prevValue = make([][]int, 9)
-	for a := range table {
-		table[a] = make([]int, 9)
-		orig[a] = make([]bool, 9)
-		prevValue[a] = make([]int, 9)
-	}
-
 	parseSudoku()
 
+	for i := range table {
+		solve(i)
+	}
+}
+
+func solve(tableNum int) {
 	row, col, solved := 0, 0, false
 
 	start := time.Now()
 
 	for !solved {
-		if row == 8 && col == 8 && validValue(8, 8, table[row][col]) {
+		if row == 8 && col == 8 && validValue(tableNum, 8, 8, table[tableNum][row][col]) {
 			solved = true
 			break
 		}
-		for orig[row][col] {
+		for orig[tableNum][row][col] {
 			moveNextSpace(&row, &col)
 		}
-		if table[row][col] == 0 {
-			table[row][col] = 1
+		if table[tableNum][row][col] == 0 {
+			table[tableNum][row][col] = 1
 		}
-		if !validValue(row, col, table[row][col]) || prevValue[row][col] == table[row][col] {
-			if table[row][col] >= 9 {
-				table[row][col] = 0
-				prevValue[row][col] = 0
+		if !validValue(tableNum, row, col, table[tableNum][row][col]) || prevValue[tableNum][row][col] == table[tableNum][row][col] {
+			if table[tableNum][row][col] >= 9 {
+				table[tableNum][row][col] = 0
+				prevValue[tableNum][row][col] = 0
 				moveLastSpace(&row, &col)
-				for orig[row][col] {
+				for orig[tableNum][row][col] {
 					moveLastSpace(&row, &col)
 				}
-				table[row][col]++
+				table[tableNum][row][col]++
 			} else {
-				table[row][col]++
+				table[tableNum][row][col]++
 			}
 		} else {
-			prevValue[row][col] = table[row][col]
+			prevValue[tableNum][row][col] = table[tableNum][row][col]
 			moveNextSpace(&row, &col)
-
 		}
 	}
 	end := time.Now()
 	fmt.Printf("Time taken %f\n", end.Sub(start).Seconds())
-	printTable()
+	printTable(tableNum)
 }
 
-func validValue(i int, j int, value int) bool {
-	rowContain := rowContains(i, j, value)
-	colContain := colContains(i, j, value)
-	boxContain := boxContains(i, j, value)
+func validValue(tableNum int, i int, j int, value int) bool {
+	rowContain := rowContains(tableNum, i, j, value)
+	colContain := colContains(tableNum, i, j, value)
+	boxContain := boxContains(tableNum, i, j, value)
 	validVal := value > 0 && value <= 9 && !rowContain && !colContain && !boxContain
 
 	return validVal
 }
 
-func rowContains(rowNum int, colNum int, value int) bool {
-	for i, v := range table[rowNum] {
+func rowContains(tableNum int, rowNum int, colNum int, value int) bool {
+	for i, v := range table[tableNum][rowNum] {
 		if i != colNum && v == value {
 			return true
 		}
@@ -83,9 +79,9 @@ func rowContains(rowNum int, colNum int, value int) bool {
 	return false
 }
 
-func colContains(rowNum int, colNum int, value int) bool {
-	for i := range table {
-		if i != rowNum && table[i][colNum] == value {
+func colContains(tableNum int, rowNum int, colNum int, value int) bool {
+	for i := range table[tableNum] {
+		if i != rowNum && table[tableNum][i][colNum] == value {
 			return true
 		}
 	}
@@ -100,30 +96,50 @@ func parseSudoku() {
 
 	rows := strings.Split(s, "\r\n")
 
+	table = make([][][]int, len(rows)/9)
+	orig = make([][][]bool, len(rows)/9)
+	prevValue = make([][][]int, len(rows)/9)
+
+	for a := range table {
+		table[a] = make([][]int, 9)
+		orig[a] = make([][]bool, 9)
+		prevValue[a] = make([][]int, 9)
+		for b := range table[a] {
+			table[a][b] = make([]int, 9)
+			orig[a][b] = make([]bool, 9)
+			prevValue[a][b] = make([]int, 9)
+		}
+	}
+
 	for i, s := range rows {
+		tableNum, rowNum := i/9, i%9
+
+		if s == "" {
+			continue
+		}
 		cols := strings.Split(s, " ")
 		for j, s1 := range cols {
+			if s1 == "" {
+				continue
+			}
 			val, err := strconv.Atoi(s1)
 			check(err)
-			table[i][j] = val
-			prevValue[i][j] = val
-			if val != 0 {
-				orig[i][j] = true
-			} else {
-				orig[i][j] = false
-			}
+
+			table[tableNum][rowNum][j] = val
+			prevValue[tableNum][rowNum][j] = val
+			orig[tableNum][rowNum][j] = val != 0
 		}
 	}
 }
 
-func boxContains(row int, col int, value int) bool {
+func boxContains(tableNum, row int, col int, value int) bool {
 	validRows := []int{(row / 3) * 3, (row/3)*3 + 1, (row/3)*3 + 2}
 	validCols := []int{(col / 3) * 3, (col/3)*3 + 1, (col/3)*3 + 2}
 
 	for _, v := range validRows {
 		for _, u := range validCols {
 			check1 := !(v == row && u == col)
-			check3 := table[v][u] == value
+			check3 := table[tableNum][v][u] == value
 
 			boxVal := check1 && check3
 			if boxVal {
@@ -158,13 +174,12 @@ func check(e error) {
 	}
 }
 
-func printTable() {
-	for _, v := range table {
+func printTable(tableNum int) {
+	for _, v := range table[tableNum] {
 		for _, v1 := range v {
 			fmt.Printf("%d ", v1)
 		}
 		fmt.Println()
-
 	}
 	fmt.Println()
 	fmt.Println()
